49ce93bdcacb9e5dac315fca3c165ca7a2dc2113
Adkham <adhammamedov@mail.ru>
2021-08-12 18:32:37 +0500

    Start router setup

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import path from 'path'
+
+import homeRouter from './routes/home.js'
+import notesRouter from './routes/notes.js'
 
 const app = express()
 const port = process.env.PORT ?? 3000
+const __dirname = path.resolve()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug')
+app.use('/static', express.static(__dirname + '/public'))
+
+app.use('/', homeRouter)
+app.use('/notes', notesRouter)
 
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`))
-
-
-// node-express
---
+++ routes/home.js
@@ -0,0 +1,7 @@
+// app.get('/', (req, res) => res.render('index'))
+import express from 'express'
+const router = express.Router()
+
+router.get('/', (req, res) => res.render('index'))
+
+export default router
